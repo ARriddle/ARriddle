a0625d58a9614d8b217c1ab058837df62657b4eb
Thibault Serti <thibault.serti@gmail.com>
2020-06-02 15:26:53 +0200

    Add apropos activity and menu

---
+++ app/src/main/java/fr/ec/arridle/fragments/MainFragment.kt
@@ -0,0 +1,24 @@
+package fr.ec.arridle.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import fr.ec.arridle.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
