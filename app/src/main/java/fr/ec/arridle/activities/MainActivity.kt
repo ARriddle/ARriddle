9761a65d460d09189285194198b3f2f620ec1854
Thibault Serti <thibault.serti@gmail.com>
2020-06-04 16:59:49 +0200

    Replace activities by fragments
    It's a big commit. I changed the way to think the app. Now we're only
    using one activityand a navigation controller to navigate between
    fragments.
    I changed the navigation drawer so it overlays the appbar.

--- app/src/main/java/fr/ec/arridle/activities/MainActivity.kt
+++ app/src/main/java/fr/ec/arridle/activities/MainActivity.kt
@@ -1,17 +1,47 @@
 package fr.ec.arridle.activities
 
 import android.os.Bundle
-import android.widget.FrameLayout
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import fr.ec.arridle.R
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BasicActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        super.setupToolBar()
+        setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
         val actionBar = supportActionBar
-        actionBar?.setDisplayHomeAsUpEnabled(false)
-        val activityContainer: FrameLayout = findViewById(R.id.layout_container)
-        layoutInflater.inflate(R.layout.activity_main, activityContainer, true)
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupWithNavController(navView, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item!!,
+            this.findNavController(R.id.myNavHostFragment)
+        )
+                || super.onOptionsItemSelected(item)
+    }
+
 }
