4764b04f7dd22582dfe2f1156902ec42f4de2f59
Thibault Serti <thibault.serti@gmail.com>
2020-06-09 11:59:49 +0200

    Add manager fragments templates

---
+++ app/src/main/java/fr/ec/arridle/fragments/manager/CreateGameFragment.kt
@@ -0,0 +1,20 @@
+package fr.ec.arridle.fragments.manager
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import fr.ec.arridle.R
+
+class CreateGameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_game, container, false)
+    }
+
+}
